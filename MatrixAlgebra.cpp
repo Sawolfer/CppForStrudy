70be4b268a4ef4a628895aca0c9e2189b0fa957e
Sawolf <Savipon@yandex.ru>
2024-03-20 00:19:09 +0300

    4 task

--- MatrixAlgebra.cpp
+++ MatrixAlgebra.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <iomanip>
 
 using namespace std;
 
@@ -8,30 +9,32 @@ using namespace std;
 class Matrix{
 protected:
     int n, m;
-    vector<vector<int> > matrix;
+    vector<vector<double> > matrix;
 public:
     Matrix(int n, int m){
         this -> n = n;
         this -> m = m;
-        matrix.resize(n, vector<int>(m, 0));
+        matrix.resize(n, vector<double>(m, 0));
     }
-    Matrix(int n, int m, vector<vector<int> > matrix){
+    Matrix(int n, int m, vector<vector<double> > matrix){
         this -> n = n;
         this -> m = m;
         this -> matrix = matrix;
     }
-    vector<vector<int> > getMatrix(){
+    vector<vector<double> > getMatrix(){
         return matrix;
     }
     friend ostream& operator<<(ostream& os, const Matrix& mat) {
         for (int i = 0; i < mat.n; ++i) {
             string output;
             for (int j = 0; j < mat.m; ++j) {
-                output += to_string(mat.matrix[i][j]) + " ";
+                if (j != mat.m - 1){
+                    cout << fixed << setprecision(2) << mat.matrix[i][j] << " ";
+                } else {
+                    cout << fixed << setprecision(2) << mat.matrix[i][j];
+                }
             }
-            output.pop_back();
-            output += "\n";
-            cout << output;
+            cout << endl;
         }
         return os;
     }
@@ -45,7 +48,7 @@ public:
         return is;
     }
 
-    int getAt(int i, int j){
+    double getAt(int i, int j){
         return matrix[i][j];
     }
 
@@ -54,6 +57,15 @@ public:
             return true;
         }
         return false;
+    }
+
+    bool isEqual(Matrix other){
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                if (matrix[i][j] != other.getAt(i, j))return false;
+            }
+        }
+        return true;
     }
 
     Matrix& operator=(const Matrix& other) {
@@ -66,10 +78,10 @@ public:
     }
 
     Matrix operator+ (Matrix& other){
-        vector<vector<int> > newMatrix;
+        vector<vector<double> > newMatrix;
         if (equals(other)){
             for (int i = 0; i < n; i++){
-                vector<int> tmp;
+                vector<double> tmp;
                 newMatrix.push_back(tmp);
                 for (int j = 0; j < m; j++){
                     newMatrix[i].push_back(matrix[i][j] + other.getAt(i, j));
@@ -83,10 +95,10 @@ public:
         return Matrix(0, 0, newMatrix);
     }
     Matrix operator- (Matrix& other){
-        vector<vector<int> > newMatrix;
+        vector<vector<double> > newMatrix;
         if (equals(other)){
             for (int i = 0; i < n; i++){
-                vector<int> tmp;
+                vector<double> tmp;
                 newMatrix.push_back(tmp);
                 for (int j = 0; j < m; j++){
                     newMatrix[i].push_back(matrix[i][j] - other.getAt(i, j));
@@ -101,13 +113,13 @@ public:
 
     }
     Matrix operator* (Matrix& other){
-        vector<vector<int> > newMatrix;
+        vector<vector<double> > newMatrix;
         if (m == other.n){
             for (int i = 0; i < n; i++){
-                vector<int> tmp;
+                vector<double> tmp;
                 newMatrix.push_back(tmp);
                 for (int j = 0; j < other.m; j++){
-                    int value = 0;
+                    double value = 0;
                     for (int k = 0; k < m; k++){
                         value += getAt(i, k) * other.getAt(k, j);
                     }
@@ -123,9 +135,9 @@ public:
 
     }
     Matrix T (){
-        vector<vector<int> > newMatrix;
+        vector<vector<double> > newMatrix;
         for (int i = 0; i < m; i++){
-            vector<int> row;
+            vector<double> row;
             newMatrix.push_back(row);
             for (int j = 0; j < n; j++){
                 newMatrix[i].push_back(getAt(j, i));
@@ -134,6 +146,7 @@ public:
         Matrix toReturn(m, n, newMatrix);
         return toReturn;
     }
+
 };
 
 class SquareMatrix : public Matrix{
@@ -141,6 +154,14 @@ class SquareMatrix : public Matrix{
 public:
     SquareMatrix(int n) : Matrix(n, n){}
 
+    SquareMatrix& operator=(const SquareMatrix& other) {
+        if (this != &other) {
+            matrix = other.matrix;
+            n = other.n;
+            m = other.m;
+        }
+        return *this;
+    }
 };
 
 class IdentityMatrix : public SquareMatrix{
@@ -156,19 +177,19 @@ public:
 class EliminationMatrix : public IdentityMatrix{
 
 public:
-    EliminationMatrix(int n, int i1, int i2, Matrix mat) : IdentityMatrix(n){
+    EliminationMatrix(int n, int i1, int i2, int index,  Matrix mat) : IdentityMatrix(n){
 
-        int a = mat.getAt(i2 - 1, 0);
-        int b = mat.getAt(i1 - 1, 0);
-        int number = (b / a) * -1;
-        matrix[i1-1][i2-1] = number;
+        double a = mat.getAt(i2 - 1, index);
+        double b = mat.getAt(i1 - 1, index);
+        double number = ((double)(b / a)) * -1;
+        matrix[i1-1][index] = number;
     }
 
 };
 
 class PermutationMatrix : public IdentityMatrix {
 public:
-    PermutationMatrix(int n, int m, int i1, int i2) : IdentityMatrix(n) {
+    PermutationMatrix(int n, int i1, int i2) : IdentityMatrix(n) {
         int tmp;
         for (int i = 0; i < m; i++){
             tmp = getAt(i1-1, i);
@@ -179,33 +200,56 @@ public:
 
 };
 
-int main() {
+double det(Matrix matA, int size){
+    double determinant = 1;
+    for (int i = 0; i < size; i ++){
+        determinant *= (matA.getAt(i, i));
+    }
+    return determinant;
+}
 
+int main() {
+    int step=0;
     int n;
+    int multi = 1;
     cin >> n;
     vector<vector<int> > matrix;
-    SquareMatrix matA(n);
+    Matrix matA(n, n);
     cin >> matA;
+    for (int r = 0; r < n; r++){
+        int maxRow = r;
+        for(int i = r; i < n; i++){
+            if (abs(matA.getAt(i, r)) > abs(matA.getAt(maxRow, r))){
+                maxRow = i;
+            }
+        }
+        PermutationMatrix matP(n, r+1, maxRow+1);
+        Matrix tmp = matP * matA;
+        if (!(tmp.isEqual(matA))){
+            matA = matP * matA;
+            step++;
+            cout << "step #" << step << ": permutation\n";
+            cout << matA;
+            multi *= -1;
+        } else{
+            matA = matP * matA;
+        }
+        for (int er = r+1; er <n; er++){
+            if (matA.getAt(er, r)!=0){
+                EliminationMatrix matE(n, er+1, r+1, r, matA);
+                matA = matE * matA;
+                step++;
+                cout << "step #" << step << ": elimination\n";
+                cout << matA;
+            }
 
-    IdentityMatrix matI(3);
+        }
+    }
 
-    EliminationMatrix matE(n, 2, 1, matA);
 
-    Matrix matB = matE * matA;
-
-    PermutationMatrix matP (n, n, 2, 1);
-
-    Matrix matC = matP * matA;
-
-    cout << matI;
-    cout << matE;
-    cout << matB;
-    cout << matP;
-    cout << matC;
+    cout << "result:\n";
+    cout << fixed << setprecision(2) << det(matA, n) * multi;
 
 
     return 0;
 }
-
-
-
